8072fa4ffebaa97703ebc03cbede9433e5767ed5
AAASASN <maraenko@gmail.com>
2022-11-23 22:36:59 +0300

    - Задание выполнено

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -16,9 +16,50 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // MARK: - здесь вносим изменения в сткоку
+//        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // вместо let _ указываем название сцены (в отличие от сториборда где сцена создается другим способом)
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        // из windowScene создаем window
+        let myWindow = UIWindow(windowScene: windowScene )
+        // теперь у window назначаем корневым контороллером табБарКонтроллер
+        myWindow.rootViewController = createUITapBarContoller()
+        // делаем window ключевым - кладем его в основу нашей иерархии
+        myWindow.makeKeyAndVisible()
+        // присваиваем ссылку myWindow на window
+        self.window = myWindow
     }
 
+    
+    // напишем функцию которая будет возвращать экземпляр UITabBarController
+    func createUITapBarContoller() -> UITabBarController {
+        let tapBarController = UITabBarController()
+        // изменяем цвет
+        tapBarController.tabBar.backgroundColor = UIColor(red: 1.00, green: 0.95, blue: 0.74, alpha: 0.67)
+        // в него сразу положим два навигейшен контроллера
+        tapBarController.viewControllers = [createFirstNavigationController(), createSecondNavigationController()]
+        return tapBarController
+    }
+    
+    // напишем функцию которая будет возвращать первый UINavigationController
+    func createFirstNavigationController() -> UINavigationController {
+        let someNavigationController = UINavigationController(rootViewController: FeedViewController())
+        // у UINavigationController есть свойство .tabBarItem которое будет передано в UITabBarController
+        someNavigationController.tabBarItem = UITabBarItem(title: "Лента", image: UIImage(systemName: "text.alignleft"), tag: 0)
+        return someNavigationController
+    }
+    
+    // напишем функцию которая будет возвращать второй UINavigationController
+    func createSecondNavigationController() -> UINavigationController {
+        let someNavigationController = UINavigationController(rootViewController: ProfileViewController())
+        // у UINavigationController есть свойство .tabBarItem которое будет передано в UITabBarController
+        someNavigationController.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person.crop.circle"), tag: 1)
+        return someNavigationController
+    }
+    
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
---
+++ Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Александр Мараенко on 23.11.2022.
+//
+
+import Foundation
+
+struct Post {
+    var title = String()
+}
