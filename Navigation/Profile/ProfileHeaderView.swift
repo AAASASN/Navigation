d5d8bd38eb9b096e701a52e5506d6233f59f62b1
AAASASN <maraenko@gmail.com>
2022-12-03 19:59:24 +0300

    ДЗ выполнено, включая задание со звездочкой + исправил замечания

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -8,67 +8,88 @@
 import UIKit
 
 class ProfileHeaderView: UIView, UITextFieldDelegate {
-
+    
     var viewForImage: UIView!
-    var nameLabel = UILabel()
-    var textField = UITextField()
-    var button = UIButton()
-    var statusLabel = UILabel()
-
+    var button: UIButton!
+    var statusLabel: UILabel!
+    var nameLabel: UILabel!
+    var textField: UITextField!
+    
     private var statusText: String = ""
     
-        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.settings()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func settings() {
         
-        // настройка вью с котиком
-        // размер вью в макете не указан выставил на глаз
-        viewForImage = UIView(frame: CGRect(x: 16, y: 80, width: 110, height: 110))
-        viewForImage.layer.cornerRadius = 55
-        viewForImage.layer.borderWidth = 3
-        viewForImage.layer.borderColor = UIColor.white.cgColor
-        let imageView = UIImageView(image: UIImage(named: "cat3"))
-        imageView.frame = CGRect(x: 3, y: 3, width: 104, height: 104)
-        viewForImage.clipsToBounds = true
-        viewForImage.addSubview(imageView)
-
+        viewForImage = {
+            // настройка вью с котиком
+            // размер вью в макете не указан выставил на глаз
+            let viewForImage = UIView(frame: CGRect(x: 16, y: 80, width: 110, height: 110))
+            viewForImage.layer.cornerRadius = 55
+            viewForImage.layer.borderWidth = 3
+            viewForImage.layer.borderColor = UIColor.white.cgColor
+            let imageView = UIImageView(image: UIImage(named: "cat3"))
+            imageView.frame = CGRect(x: 3, y: 3, width: 104, height: 104)
+            viewForImage.clipsToBounds = true
+            viewForImage.addSubview(imageView)
+            return viewForImage
+        }()
         
-        // настройка кнопки
-        button = UIButton(frame: CGRect(x: 16, y: 231, width: self.frame.maxX - 32, height: 50))
-        button.layer.backgroundColor = UIColor.systemBlue.cgColor
-        // в макете указан cornerRadius = 4 по факту около 13
-        button.layer.cornerRadius = 13
-        // настройки тени в макете также не сответствуют в цифрах и по фактку, не стал подгонять
-        button.layer.shadowOffset = CGSize(width: 4, height: 4)
-        button.layer.shadowRadius = 4
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOpacity = 0.7
-        // Font кнопки в макете ни как не указан
-        button.setTitle("Show status", for: .normal)
-        button.setTitleColor(.white, for: .normal)
+        button = {
+            // настройка кнопки
+            let button = UIButton(frame: CGRect(x: 16, y: 231, width: self.frame.maxX - 32, height: 50))
+            button.layer.backgroundColor = UIColor.systemBlue.cgColor
+            // в макете указан cornerRadius = 4 по факту около 13
+            button.layer.cornerRadius = 13
+            // настройки тени в макете также не сответствуют в цифрах и по фактку, не стал подгонять
+            button.layer.shadowOffset = CGSize(width: 4, height: 4)
+            button.layer.shadowRadius = 4
+            button.layer.shadowColor = UIColor.black.cgColor
+            button.layer.shadowOpacity = 0.7
+            // Font кнопки в макете ни как не указан
+            button.setTitle("Show status", for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            return button
+        }()
         
+        statusLabel = {
+            // настройки лейбла со статусом
+            // в макете не указана высота лейбла задал 14 как размер font
+            let statusLabel = UILabel(frame: CGRect(x: 142, y: 158, width: self.frame.maxX - 142 - 16, height: 16))
+            statusLabel.text = "Wating for something..."
+            statusLabel.textColor = UIColor.gray
+            statusLabel.font = UIFont.systemFont(ofSize: CGFloat(14), weight: .regular)
+            return statusLabel
+        }()
         
-        // настройки лейбла со статусом
-        // в макете не указана высота лейбла задал 14 как размер font
-        statusLabel = UILabel(frame: CGRect(x: 142, y: 158, width: self.frame.maxX - 142 - 16, height: 16))
-        statusLabel.text = "Wating for something..."
-        statusLabel.textColor = UIColor.gray
-        statusLabel.font = UIFont.systemFont(ofSize: CGFloat(14), weight: .regular)
-
+        nameLabel = {
+            // настройки лейбла со именем
+            let nameLabel = UILabel(frame: CGRect(x: 142, y: 91, width: self.frame.maxX - 142 - 16, height: 18))
+            nameLabel.text = "Itchy Cat"
+            nameLabel.textColor = UIColor.black
+            nameLabel.font = UIFont.systemFont(ofSize: CGFloat(18), weight: .bold)
+            return nameLabel
+        }()
         
-        // настройки лейбла со именем
-        nameLabel = UILabel(frame: CGRect(x: 142, y: 91, width: self.frame.maxX - 142 - 16, height: 18))
-        nameLabel.text = "Itchy Cat"
-        nameLabel.textColor = UIColor.black
-        nameLabel.font = UIFont.systemFont(ofSize: CGFloat(18), weight: .bold)
-
-        //настройки textField
-        textField = UITextField(frame: CGRect(x: 142, y: 180, width: self.frame.maxX - 142 - 16, height: 40))
-        textField.layer.borderWidth = 1
-        textField.layer.borderColor = UIColor.black.cgColor
-        textField.layer.cornerRadius = 12
-        textField.font = UIFont.systemFont(ofSize: CGFloat(15), weight: .regular)
-        textField.layer.backgroundColor = UIColor.white.cgColor
-        textField.indent(size: 8)
+        textField = {
+            //настройки textField
+            let textField = UITextField(frame: CGRect(x: 142, y: 180, width: self.frame.maxX - 142 - 16, height: 40))
+            textField.layer.borderWidth = 1
+            textField.layer.borderColor = UIColor.black.cgColor
+            textField.layer.cornerRadius = 12
+            textField.font = UIFont.systemFont(ofSize: CGFloat(15), weight: .regular)
+            textField.layer.backgroundColor = UIColor.white.cgColor
+            textField.indent(size: 8)
+            return textField
+        }()
+        
         
         // добавляем кнопку
         self.addSubview(button)
@@ -89,7 +110,7 @@ class ProfileHeaderView: UIView, UITextFieldDelegate {
         textField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
     }
     
-
+    
     // обработчик таргета
     @objc
     func buttonPressed() {
@@ -110,8 +131,8 @@ class ProfileHeaderView: UIView, UITextFieldDelegate {
         self.endEditing(true)
         return false
     }
-
-
+    
+    
 }
 
 extension UITextField {
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,8 +8,10 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
-    let profileHeaderView = ProfileHeaderView()
+    
+    // let profileHeaderView = ProfileHeaderView(frame: CGRect())
+     
+    var profileHeaderView = ProfileHeaderView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,10 +19,15 @@ class ProfileViewController: UIViewController {
         self.navigationController?.navigationBar.backgroundColor = UIColor.white
         self.navigationItem.title = "Profile"
         
+        
+        
+        profileHeaderView = ProfileHeaderView(frame: CGRect(x: 0, y: 0, width: Int(self.view.frame.maxX), height: Int(self.view.frame.maxY)))
         // в макете указан цвет .gray но по факту это явно .systemGray4 - пришлось сделать systemGray5
         // что бы не сливаться с клавиатурой и не засвечивать цвет текста статуса
         view.backgroundColor = UIColor.systemGray5
         
+        //profileHeaderView.frame = view.frame
+
         view.addSubview(profileHeaderView)
         
     }
@@ -28,8 +35,8 @@ class ProfileViewController: UIViewController {
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
 
-        profileHeaderView.frame = view.frame
-        profileHeaderView.settings()
+        // profileHeaderView.frame = view.frame
+        //profileHeaderView.settings()
     }
 
 }
