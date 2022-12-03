494c2b8d66d1418c051eed0256567b1d6b0ddd63
AAASASN <maraenko@gmail.com>
2022-12-03 20:14:42 +0300

    ДЗ выполнено, включая задание со звездочкой + исправил замечания + extention

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -132,12 +132,5 @@ class ProfileHeaderView: UIView, UITextFieldDelegate {
         return false
     }
     
-    
 }
 
-extension UITextField {
-    func indent(size:CGFloat) {
-        self.leftView = UIView(frame: CGRect(x: self.frame.minX, y: self.frame.minY, width: size, height: self.frame.height))
-        self.leftViewMode = .always
-    }
-}
