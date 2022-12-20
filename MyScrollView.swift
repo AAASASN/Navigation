524cdd133873d2df456d7e5aae3c54b390be1f12
AAASASN <maraenko@gmail.com>
2022-12-21 02:46:42 +0300

    Задание на проверку с проблемным лендскейпным вводом

--- MyScrollView.swift
+++ MyScrollView.swift
@@ -153,7 +153,5 @@ class MyScrollView: UIScrollView {
             return button
         }()
         
-        
-
     }
 }
--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -9,23 +9,24 @@ import Foundation
 import UIKit
 
 class LogInViewController: UIViewController {
-     
+    
     var scrollView = MyScrollView()
+    var tap: UITapGestureRecognizer! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        // self.navigationController?.navigationBar.isHidden = true
-
+        
         scrollView.settings()
+        
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(scrollView)
         NSLayoutConstraint.activate([
             scrollView.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor),
-            scrollView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
-            scrollView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            scrollView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            scrollView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
         ])
         
         scrollView.button.addAction(UIAction(handler: { _ in
@@ -33,73 +34,78 @@ class LogInViewController: UIViewController {
             self.navigationController?.pushViewController(profileViewController, animated: true)
         }), for: .touchUpInside)
         
+        self.scrollView.contentSize = CGSize(width: self.view.frame.maxX - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right, height: 522 - self.view.safeAreaInsets.top)
+        
         registerForKeyboardNotifications()
         
-        // self.scrollView.contentSize = CGSize(width: view.frame.maxX, height: view.frame.maxY + 100)
-
+        tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        
+        self.scrollView.contentSize = CGSize(width: self.view.frame.maxX - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right, height: 522)
+        
+        viewWillLayoutSubviews()
+    }
+    
 }
 
 // это расширение возволяет скрыть клавиатуру при косании вне TextView или TextField.
 // Данное решение подходит для случая когда TextView или TextField расположены внутри ScrollView
 extension LogInViewController {
-    
+
     // активируем режим отслеживания нажатия на scrollView
-    func hideKeyboard() {
-        //
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+    func registerTapGestureRecognizerForHideKeyboard() {
         scrollView.addGestureRecognizer(tap)
+        viewWillLayoutSubviews()
     }
 
     // скрываем клавиатуру с scrollView и удаляем gestureRecognizers что бы scrollView работал в обычном режиме
     @objc func dismissKeyboard() {
         // скрываем клавиатуру
         scrollView.endEditing(true)
+        scrollView.removeGestureRecognizer(tap)
     }
-    
+
 
     // этот метод будет вызван в viewDidLoad, он регистрирует наблюдателей которые...
     func registerForKeyboardNotifications() {
-        
+
         // ... будет реагировать на появление клавиатуры
         NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        
+
         // ... будет реагировать на начало изчезновения клавиатуры
         NotificationCenter.default.addObserver(self, selector: #selector(kbWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        // ... будет реагировать после изчезновения клавиатуры
-        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
-        
+
     }
-    
-    // удаление наблюдателей когда они уже не нужны
-    func removeKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
+
     // функция вычисляет размер сдвига и обращась к scrollView задает растояние сдвига contentOffset
     @objc func kbWillShow(_ notification: Notification) {
-        
+
         // запрашиваем параметры клавиатуры
         let userInfo = notification.userInfo
         let kbFrameSize = (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        
-        // сдвигаем tableView на contentOffset
-        scrollView.contentOffset = CGPoint(x: 0, y: kbFrameSize.height - 80 )
-        
+
+        // сдвигаем tableView на contentOffset минус некоторое значение зависящее от
+        // размера экрана
+        let yValue = 522 + kbFrameSize.height - view.frame.maxY
+        scrollView.contentOffset = CGPoint(x: 0, y: yValue)
+
         // в тот момент когда появилась клавиатура зарегистрируем нажатие UITapGestureRecognizer
-        hideKeyboard()
+        registerTapGestureRecognizerForHideKeyboard()
+
     }
-    
+
     // функция обращась к tableView задает растояние сдвига contentOffset равное 0
     @objc func kbWillHide() {
-        scrollView.contentOffset = CGPoint(x: 0, y: 0) // CGPoint.zero
+        // сдвигаем tableView на contentOffset
+        scrollView.contentOffset = CGPoint(x: 0, y: -self.view.safeAreaInsets.top)
     }
-    
-    @objc func kbDidHide() {
-        // удаляем все экзкмпляры UITapGestureRecognizer которые привязаны к scrollView
-        scrollView.gestureRecognizers?.forEach(scrollView.removeGestureRecognizer)
-    }
-    
+
 }
