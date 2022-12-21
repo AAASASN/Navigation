f44f316284322365d0eea1053e13f119bb02320d
AAASASN <maraenko@gmail.com>
2022-12-21 17:10:50 +0300

    решение на проверку с криво реализованными отступами в лендсткейпном режиме

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -15,7 +15,7 @@ class LogInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = .white
         
         scrollView.settings()
@@ -23,33 +23,63 @@ class LogInViewController: UIViewController {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(scrollView)
         NSLayoutConstraint.activate([
-            scrollView.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor),
-            scrollView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+            
+            // вариант при котором горизонтальный режим выгрядит хорошо, но нет реакции при нажатии
+            // на текстФилды и кнопку
+//            scrollView.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor),
+//            scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+//            scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+//            scrollView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor)
+            
+            // вариант при котором горизонтальный режим выгрядит плохо и есть заступы за сейфэрию, но
+            // при нажатии на текстФилды появляется клавиатура но не работает кнопка
+            scrollView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: view.safeAreaInsets.top),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: view.safeAreaInsets.left),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: view.safeAreaInsets.right),
+            scrollView.widthAnchor.constraint(equalTo: self.view.widthAnchor)
+            
+
+            
         ])
+        
         
         scrollView.button.addAction(UIAction(handler: { _ in
             let profileViewController = ProfileViewController()
             self.navigationController?.pushViewController(profileViewController, animated: true)
         }), for: .touchUpInside)
         
-        self.scrollView.contentSize = CGSize(width: self.view.frame.maxX - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right, height: 522 - self.view.safeAreaInsets.top)
-        
         registerForKeyboardNotifications()
         
         tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        
+        viewWillLayoutSubviews()
+
+        // при первом отображении экрана отсупы сейфЭрии всегда по нулям
+        
+        print("Screen viewWillAppear ")
+        print("top = \(self.view.safeAreaInsets.top)")
+        print("bottom = \(self.view.safeAreaInsets.bottom)")
+        print("left = \(self.view.safeAreaInsets.left)")
+        print("right = \(self.view.safeAreaInsets.right)")
+        
         self.navigationController?.navigationBar.isHidden = true
+        self.scrollView.contentSize = CGSize(width: self.view.frame.maxX, height: 522 - self.view.safeAreaInsets.top)
+        
     }
     
     override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
         
-        self.scrollView.contentSize = CGSize(width: self.view.frame.maxX - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right, height: 522)
+        // при повороте экрана отступы сейфЭрии высчитываются коректно
+        print("Screen didRotate ")
+        print("top = \(self.view.safeAreaInsets.top)")
+        print("bottom = \(self.view.safeAreaInsets.bottom)")
+        print("left = \(self.view.safeAreaInsets.left)")
+        print("right = \(self.view.safeAreaInsets.right)")
+
+        self.scrollView.contentSize = CGSize(width: self.view.frame.maxX - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right, height: 522 - self.view.safeAreaInsets.top)
         
         viewWillLayoutSubviews()
     }
