5f27ca451aad5b2d9412a415d14ac93eafbccaba
AAASASN <maraenko@gmail.com>
2023-01-24 04:39:48 +0300

    - Домашнее задание "Жесты и анимация" выполнено - поработал над чистотой кода

--- Navigation/Profile/ProfileViewController/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileViewController/ProfileHeaderView.swift
@@ -10,10 +10,15 @@ import UIKit
 class ProfileHeaderView: UIView, UITextFieldDelegate {
     
     var avatarImageView: UIView!
+    var imageViewInAvatarImageView: UIImageView!
     var fullNameLabel: UILabel!
     var statusLabel: UILabel!
     var statusTextField: UITextField!
     var setStatusButton: UIButton!
+    
+    var tapOnAvatarImageView: UITapGestureRecognizer!
+    
+    var clouserForAnimationOnProfileViewController: (()->Void)!
     
     private var statusText: String = ""
     
@@ -21,102 +26,140 @@ class ProfileHeaderView: UIView, UITextFieldDelegate {
         super.init(frame: frame)
         self.settings()
     }
-        
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     func settings() {
         
-        avatarImageView = {
-            let viewForImage = UIView(frame: .zero)
-            viewForImage.translatesAutoresizingMaskIntoConstraints = false
-            self.addSubview(viewForImage)
-            NSLayoutConstraint.activate([viewForImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
-                                         viewForImage.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
-                                         viewForImage.widthAnchor.constraint(equalToConstant: 110),
-                                         viewForImage.heightAnchor.constraint(equalToConstant: 110)])
-            viewForImage.layer.cornerRadius = 55
-            viewForImage.layer.borderWidth = 3
-            viewForImage.layer.borderColor = UIColor.systemGray5.cgColor
-            let imageView = UIImageView(image: UIImage(named: "cat3"))
-            imageView.frame = CGRect(x: 3, y: 3, width: 104, height: 104)
-            viewForImage.clipsToBounds = true
-            viewForImage.addSubview(imageView)
-            return viewForImage
-        }()
-        
-        fullNameLabel = {
-            let nameLabel = UILabel(frame: .zero)
-            nameLabel.translatesAutoresizingMaskIntoConstraints = false
-            self.addSubview(nameLabel)
-            NSLayoutConstraint.activate([nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
-                                         nameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 27),
-                                         nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
-                                         nameLabel.heightAnchor.constraint(equalToConstant: 18)])
-            nameLabel.text = "Itchy Cat"
-            nameLabel.textColor = UIColor.black
-            nameLabel.font = UIFont.systemFont(ofSize: CGFloat(18), weight: .bold)
-            return nameLabel
-        }()
-        
-        setStatusButton = {
-            let button = UIButton(frame: .zero)
-            button.translatesAutoresizingMaskIntoConstraints = false
-            self.addSubview(button)
-            NSLayoutConstraint.activate([button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
-                                         button.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 56),
-                                         button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
-                                         button.heightAnchor.constraint(equalToConstant: 50),
-                                         button.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20)])
-            button.layer.backgroundColor = UIColor.systemBlue.cgColor
-            button.layer.cornerRadius = 13
-            button.layer.shadowOffset = CGSize(width: 4, height: 4)
-            button.layer.shadowRadius = 4
-            button.layer.shadowColor = UIColor.black.cgColor
-            button.layer.shadowOpacity = 0.7
-            button.setTitle("Show status", for: .normal)
-            button.setTitleColor(.white, for: .normal)
-            return button
-        }()
-        
-        statusLabel = {
-            let statusLabel = UILabel(frame: .zero)
-            statusLabel.translatesAutoresizingMaskIntoConstraints = false
-            self.addSubview(statusLabel)
-            NSLayoutConstraint.activate([statusLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
-                                         statusLabel.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -74),
-                                         statusLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
-                                         statusLabel.heightAnchor.constraint(equalToConstant: 14)])
-            statusLabel.text = "Wating for something..."
-            statusLabel.textColor = UIColor.gray
-            statusLabel.font = UIFont.systemFont(ofSize: CGFloat(14), weight: .regular)
-            return statusLabel
-        }()
-                
-        statusTextField = {
-            let textField = UITextField(frame: .zero)
-            textField.translatesAutoresizingMaskIntoConstraints = false
-            self.addSubview(textField)
-            NSLayoutConstraint.activate([textField.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
-                                         textField.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -20),
-                                         textField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
-                                         textField.heightAnchor.constraint(equalToConstant: 40)])
-            textField.layer.borderWidth = 1
-            textField.layer.borderColor = UIColor.black.cgColor
-            textField.layer.cornerRadius = 12
-            textField.font = UIFont.systemFont(ofSize: CGFloat(15), weight: .regular)
-            textField.layer.backgroundColor = UIColor.white.cgColor
-            textField.indent(size: 8)
-            textField.placeholder = "Set your status..."
-            return textField
-        }()
+        createViewElements()
+        switchAutoresizingMaskIntoConstraints()
+        addSubviewsToSelf()
+        setNSLayoutConstraint()
         
         self.statusTextField.delegate = self
         
         setStatusButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         statusTextField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        
+        tapOnAvatarImageView = UITapGestureRecognizer()
+        tapOnAvatarImageView.addTarget(self, action: #selector(tapOnAvatarImageViewFunc(_:)))
+        avatarImageView.addGestureRecognizer(tapOnAvatarImageView)
+        
+        // centerOfScreen = CGPoint()
     }
+    
+    
+    func createViewElements() {
+        
+        avatarImageView = {
+            let avatarImageView = UIView(frame: .zero)
+            avatarImageView.layer.cornerRadius = 55
+            avatarImageView.layer.borderWidth = 3
+            avatarImageView.layer.borderColor = UIColor.systemGray5.cgColor
+            avatarImageView.clipsToBounds = true
+            return avatarImageView
+        }()
+        
+        imageViewInAvatarImageView = {
+            let imageViewInAvatarImageView = UIImageView(image: UIImage(named: "cat3"))
+            imageViewInAvatarImageView.frame = CGRect(x: 3, y: 3, width: 104, height: 104)
+            return imageViewInAvatarImageView
+        }()
+        
+        
+        fullNameLabel = {
+            let fullNameLabel = UILabel(frame: .zero)
+            fullNameLabel.text = "Itchy Cat"
+            fullNameLabel.textColor = UIColor.black
+            fullNameLabel.font = UIFont.systemFont(ofSize: CGFloat(18), weight: .bold)
+            return fullNameLabel
+        }()
+        
+        setStatusButton = {
+            let setStatusButton = UIButton(frame: .zero)
+            setStatusButton.layer.backgroundColor = UIColor.systemBlue.cgColor
+            setStatusButton.layer.cornerRadius = 13
+            setStatusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+            setStatusButton.layer.shadowRadius = 4
+            setStatusButton.layer.shadowColor = UIColor.black.cgColor
+            setStatusButton.layer.shadowOpacity = 0.7
+            setStatusButton.setTitle("Show status", for: .normal)
+            setStatusButton.setTitleColor(.white, for: .normal)
+            return setStatusButton
+        }()
+        
+        statusLabel = {
+            let statusLabel = UILabel(frame: .zero)
+            statusLabel.text = "Wating for something..."
+            statusLabel.textColor = UIColor.gray
+            statusLabel.font = UIFont.systemFont(ofSize: CGFloat(14), weight: .regular)
+            return statusLabel
+        }()
+        
+        statusTextField = {
+            let statusTextField = UITextField(frame: .zero)
+            statusTextField.layer.borderWidth = 1
+            statusTextField.layer.borderColor = UIColor.black.cgColor
+            statusTextField.layer.cornerRadius = 12
+            statusTextField.font = UIFont.systemFont(ofSize: CGFloat(15), weight: .regular)
+            statusTextField.layer.backgroundColor = UIColor.white.cgColor
+            statusTextField.indent(size: 8)
+            statusTextField.placeholder = "Set your status..."
+            return statusTextField
+        }()
+        
+    }
+    
+    func switchAutoresizingMaskIntoConstraints() {
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        setStatusButton.translatesAutoresizingMaskIntoConstraints = false
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        statusTextField.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func addSubviewsToSelf() {
+        self.addSubview(fullNameLabel)
+        self.addSubview(setStatusButton)
+        self.addSubview(statusLabel)
+        self.addSubview(statusTextField)
+        self.addSubview(avatarImageView)
+        avatarImageView.addSubview(imageViewInAvatarImageView)
+    }
+    
+    func setNSLayoutConstraint() {
+        
+        NSLayoutConstraint.activate([avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+                                     avatarImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+                                     avatarImageView.widthAnchor.constraint(equalToConstant: 110),
+                                     avatarImageView.heightAnchor.constraint(equalToConstant: 110),
+                                     
+                                     fullNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
+                                     fullNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 27),
+                                     fullNameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+                                     fullNameLabel.heightAnchor.constraint(equalToConstant: 18),
+                                     
+                                     setStatusButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+                                     setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 56),
+                                     setStatusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+                                     setStatusButton.heightAnchor.constraint(equalToConstant: 50),
+                                     setStatusButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
+                                     
+                                     statusLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
+                                     statusLabel.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -74),
+                                     statusLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+                                     statusLabel.heightAnchor.constraint(equalToConstant: 14),
+                                     
+                                     statusTextField.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
+                                     statusTextField.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -20),
+                                     statusTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+                                     statusTextField.heightAnchor.constraint(equalToConstant: 40)
+                                     
+                                    ])
+    }
+    
     
     @objc
     func buttonPressed() {
@@ -136,6 +179,13 @@ class ProfileHeaderView: UIView, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.endEditing(true)
         return false
+    }
+    
+    @objc func tapOnAvatarImageViewFunc(_ gestureRecognizer: UITapGestureRecognizer) {
+
+        // вызов клоужера для отработки анимации на ProfileViewController
+        clouserForAnimationOnProfileViewController()
+        
     }
     
 }
--- Navigation/Profile/ProfileViewController/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController/ProfileViewController.swift
@@ -13,10 +13,23 @@ class ProfileViewController: UIViewController {
     var tableView: UITableView!
     var tap: UITapGestureRecognizer! = nil
     
+    var profileHeaderView: ProfileHeaderView!
+    
+    // animation elements
+    var whiteViewForAnimation: UIView!
+    var imageViewForAnimationView: UIImageView!
+    var animatingView: UIView!
+    var closeButton: UIButton!
+    var smallWhiteView: UIView!
+    
+    //
+    var tempY: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableViewSettings()
+        profileHeaderViewSettings()
         postArray = PostArray.createPostArray()
         tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
         
@@ -29,7 +42,7 @@ class ProfileViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         tableView.reloadData()
-
+        
         removeKeyboardNotifications()
     }
     
@@ -41,11 +54,13 @@ class ProfileViewController: UIViewController {
         tableView = UITableView(frame: .zero)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
+        
         NSLayoutConstraint.activate([tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
                                      tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
                                      tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
                                      tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
                                     ])
+        
         tableView.backgroundColor = .white
         tableView.separatorInset.left = .zero
         tableView.delegate = self
@@ -77,7 +92,7 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
             cell.image2.image = UIImage(named: PhotoGallery.getPhotoArray()[1])
             cell.image3.image = UIImage(named: PhotoGallery.getPhotoArray()[2])
             cell.image4.image = UIImage(named: PhotoGallery.getPhotoArray()[3])
-
+            
             cellForReturn = cell
         }
         
@@ -97,8 +112,10 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         if section == 0 {
-            let profileHeaderView = ProfileHeaderView(frame: .zero)
+            guard let profileHeaderView = self.profileHeaderView else { return UIView(frame: .zero) }
+            
             profileHeaderView.backgroundColor = .white
+            
             return  profileHeaderView
         }
         if section == 1 {
@@ -152,6 +169,171 @@ extension ProfileViewController {
     @objc func kbWillShow(_ notification: Notification) {
         registerTapGestureRecognizerForHideKeyboard()
     }
+}
+
+
+// MARK: - Animation part
+extension ProfileViewController {
+
+    func profileHeaderViewSettings() {
+        
+        profileHeaderView = ProfileHeaderView(frame: .zero)
+        
+        // передача замыкания для отработки анимации
+        profileHeaderView.clouserForAnimationOnProfileViewController = {
+            
+            // отрисовываем новые элементы анимации и добавляем их на основной вью контроллера
+            self.prepareAnimationElements()
+
+            //
+            self.startAnimation()
+            
+        }
+    }
+    
+    func prepareAnimationElements() {
+        
+        // белое вью для скрытия всех элементов на корневом вью контроллера
+        whiteViewForAnimation = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        whiteViewForAnimation.backgroundColor = .white
+        whiteViewForAnimation.alpha = 0
+        
+        // картинка в автарке
+        imageViewForAnimationView = profileHeaderView.imageViewInAvatarImageView
+        
+        
+        // вью аватарки
+        animatingView = {
+            let avatarImageView = UIView(frame: CGRect(x: 16, y: 16, width: 110, height: 108))
+            avatarImageView.layer.cornerRadius = avatarImageView.frame.width/2
+            avatarImageView.layer.borderWidth = 3
+            avatarImageView.layer.borderColor = UIColor.systemGray5.cgColor
+            avatarImageView.clipsToBounds = true
+            return avatarImageView
+        }()
+        // устанавливаем кординату "у" в соответствии с положением хедера таблицы относительно view
+        animatingView.frame.origin.y += self.view.safeAreaInsets.top + self.getPositionOffset()
+        // сохраняем значение у в переменную для хранения и дальнейшего использования при обратной анимации
+        tempY = animatingView.frame.origin.y
+        
+        
+        //          self.profileHeaderView.addSubview(animatingView)
+        
+        closeButton = {
+            let closeButton = UIButton(frame: CGRect(x: self.view.frame.size.width - 56,
+                                                     y: self.view.safeAreaInsets.top + 16,
+                                                     width: 40,
+                                                     height: 40))
+            closeButton.backgroundColor = .white
+            closeButton.layer.cornerRadius = 20
+            let closeButtonAction = UIAction { _ in
+                self.startReversAnimation()
+            }
+            closeButton.addAction(closeButtonAction, for: .allTouchEvents)
+            
+            let imageView = UIImageView(image: UIImage(systemName: "multiply.circle"))
+            imageView.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+            closeButton.addSubview(imageView)
+            //closeButton.center = imageView.center
+            closeButton.clipsToBounds = true
+            return closeButton
+        }()
+        
+        //
+        smallWhiteView = UIView(frame: CGRect(x: self.view.frame.size.width - 56,
+                                              y: self.view.safeAreaInsets.top + 16,
+                                              width: 40,
+                                              height: 40))
+        smallWhiteView.backgroundColor = .white
+        
+        self.view.addSubview(self.whiteViewForAnimation)
+        self.view.addSubview(animatingView)
+        animatingView.addSubview(imageViewForAnimationView)
+        self.view.addSubview(closeButton)
+        self.view.addSubview(smallWhiteView)
+    }
+    
+    func startAnimation() {
+        
+        // скрывает аватарку в profileHeaderView перед началом анимации
+        self.profileHeaderView.avatarImageView.isHidden = true
+
+        UIView.animate(withDuration: 0.5, delay: 0) {
+            
+            self.animatingView.center.y = self.view.center.y - (self.view.frame.width - 32)/2
+            
+            self.animatingView.frame.size.height = self.view.frame.width - 32
+            self.animatingView.frame.size.width = self.view.frame.width - 32
+            
+            self.animatingView.layer.cornerRadius = 0 // (self.view.frame.width - 32)/2
+            self.animatingView.layer.borderWidth = 7
+            self.animatingView.layer.borderColor = UIColor.systemGray3.cgColor
+            
+            self.imageViewForAnimationView.frame.size.height = self.view.frame.width - 32
+            self.imageViewForAnimationView.frame.size.width = self.view.frame.width - 32
+            
+            self.whiteViewForAnimation.alpha = 1
+        }
+
+        UIView.animate(withDuration: 0.3, delay: 0.5) {
+            self.smallWhiteView.alpha = 0
+        }
+        
+    }
+    
+    func startReversAnimation() {
+        
+        // анимация кнопки
+        UIView.animate(withDuration: 0.3, delay: 0) {
+            self.smallWhiteView.alpha = 1
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.3, animations: {
+            
+            self.animatingView.frame.origin.x = self.profileHeaderView.avatarImageView.frame.origin.x
+            self.animatingView.frame.origin.y = self.profileHeaderView.avatarImageView.frame.origin.x + self.view.safeAreaInsets.top + self.getPositionOffset()
+            
+            self.animatingView.frame.size.height = 108
+            self.animatingView.frame.size.width = 110
+            
+            self.animatingView.layer.cornerRadius = self.animatingView.frame.size.height/2
+            self.animatingView.layer.borderWidth = 3
+            self.animatingView.layer.borderColor = UIColor.systemGray5.cgColor
+
+            
+            self.imageViewForAnimationView.frame.size.height = 104
+            self.imageViewForAnimationView.frame.size.width = 104
+            
+            self.whiteViewForAnimation.alpha = 0
+        })
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+            
+            self.profileHeaderView.avatarImageView.addSubview(self.profileHeaderView.imageViewInAvatarImageView)
+            self.profileHeaderView.avatarImageView.isHidden = false
+            self.animatingView.isHidden = true
+        })
+
+    }
+    
+    
+    // Эта функция определяет офсет на который "сыграл" навинейшен бар при прокрутке таблицы
+    func getPositionOffset() -> CGFloat {
+        var valueForReturn: CGFloat = 0
+        let cellRect = tableView.rectForRow(at: IndexPath(row: 0, section: 0))
+        let y = cellRect.origin.y
+        let contentOffset = tableView.contentOffset
+        let y_coordinate = y - contentOffset.y // координата ячейки относительно экранных координат
+        
+        if y_coordinate >= 272 {
+            valueForReturn = 22
+        }
+        if y_coordinate < 272 && y_coordinate >= 250 {
+            valueForReturn = y_coordinate - 250
+        }
+        return valueForReturn
+    }
+    
 }
 
 
